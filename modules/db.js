32c3407056372c8a0429b9348e84db13547de050
Ilyas <ilyas.sultanov2020@list.ru>
2021-08-19 15:25:10 +0500

    modules

--- modules/db.js
+++ modules/db.js
@@ -9,6 +9,7 @@
      appId: "1:484242441762:web:65ff8193a8dc607d45ec7b",
      measurementId: "G-CWZCTZLM9F"
  };
+
  // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
  const db = firebase.firestore();
